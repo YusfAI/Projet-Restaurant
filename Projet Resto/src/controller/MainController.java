56d03413c16f861d7b0b4863ec7fae4516ca17d3
en-maiza <maissamallat05@gmail.com>
2025-12-12 22:25:55 +0100

    Update MainController.java

--- Projet Resto/src/controller/MainController.java
+++ Projet Resto/src/controller/MainController.java
@@ -1,49 +1,64 @@
-package Controller;
+package controller;
 import model.*;
-import servie.AuthentificationService;
-import exceptions.*;
+import service.AuthentificationService;
 import view.ConsoleUI;
 public class MainController {
-	private AuthentificationService authService;
-	private ConsoleUI consoleUI;
-	private User currentUser;
-	public MainController() {
-		this.authService=new AuthentificationService();
-		this.consoleUI=new ConsoleUI
-	}
-	public void startApplication() {
-		consoleUI.displayWelcome();
-		while(true) {
-			int choice=consoleUI.displayMainMenu();
-			switch (choice) {
-			case 1: controlLogin(); break;
-			case 2: controlRegistration(); break;
-			case 3: consoleUI.displayMessage("Au revoir!"); System.exit(0); break;
-			default : consoleUI.displayError("choix invalide");
-			}
-		}
-	}
-	private void controlLogin() {
-		String nom=consoleUI.prompt("Nom d'utilisateur:");
-		String password=consoleUI.promptPassword("Mot de passe:");
-		currentUser=authService.authentificate(nom,password);
-		if (currentUser==null) {
-			consoleUI.displayError("Echec d'authentification");
-			return;
-		}
-		roleController();
-	}
-	private void roleController() {
-		String role=currentUser.getRole();
-		switch(role) {
-			case "ETUDIANT": StudentMenuController studentControl=new StudentMenuController(currentUser); studentControl.showTab();break;
-			case "ADMIN": AdminMenuController adminControl=new AdminMenuController(currentUser); adminControl.showTab(); break;
-			case "STAFF" :StaffMenuController staffControl=new StaffMenuController(currentUser); staffControl.showTab(); break;
-			default: consoleUI.displayError("Role inconnu");
-		}
-	}
-	private void controlRegistration() {
-		consoleUI.displayMessage("Inscription");
-	}
+    private AuthentificationService authService;
+    private ConsoleUI consoleUI;
+    private User currentUser;
+    public MainController() {
+        this.authService = new AuthentificationService();
+        this.consoleUI = new ConsoleUI();
+    }
+    public void startApplication() {
+        consoleUI.displayWelcome();
+        while (true) {
+            int choice = consoleUI.displayMainMenu();
+            switch (choice) {
+                case 1:
+                    controlLogin();
+                    break;
+                case 2:
+                    controlRegistration();
+                    break;
+                case 3:
+                    consoleUI.displayMessage("Au revoir !");
+                    System.exit(0);
+                    break;
+                default:
+                    consoleUI.displayError("Choix invalide !");
+            }
+        }
+    }
+    private void controlLogin() {
+        String login = consoleUI.prompt("Nom d'utilisateur : ");
+        String password = consoleUI.promptPassword("Mot de passe : ");
+        currentUser = authService.authenticate(login, password);
+        if (currentUser == null) {
+            consoleUI.displayError("Échec d'authentification !");
+            return;
+        }
+		showUserMenu();
+    }
+    private void showUserMenu() {
+        String role = currentUser.getRole();
+        switch (role.toUpperCase()) {
+            case "ETUDIANT":
+                new StudentMenuController(currentUser).showTab();
+                break;
+            case "ADMIN":
+                new AdminMenuController(currentUser).showTab();
+                break;
+            case "STAFF":
+                new StaffMenuController(currentUser).showTab();
+                break;
+
+            default:
+                consoleUI.displayError("Rôle inconnu !");
+        }
+    }
+    private void controlRegistration() {
+        consoleUI.displayMessage("Inscription");
+    }
 }
 
