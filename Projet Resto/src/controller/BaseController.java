52ef9132e777d1525d65dfab5e2848f7f1649477
en-maiza <maissamallat05@gmail.com>
2025-12-12 22:32:52 +0100

    Update BaseController.java

--- Projet Resto/src/controller/BaseController.java
+++ Projet Resto/src/controller/BaseController.java
@@ -1,24 +1,23 @@
-package Controller;
+package controller;
 import model.User;
 import service.*;
 import view.*;
-
 public abstract class BaseController {
-	protected User currentUser;
+    protected User currentUser;
     protected AuthenticationService authService;
-    protected ReservationService reservService;
+    protected ReservationService reservationService;
     protected ConsoleUI consoleUI;
     public BaseController(User user) {
-    	this.currentUser=user;
-    	this.authService=new AuthentificationService();
-    	this.reservService=new ReservationService();
-    	this.consoleUI=new ConsoleUI();
+        this.currentUser = user;
+        this.authService = new AuthenticationService();
+        this.reservationService = new ReservationService();
+        this.consoleUI = new ConsoleUI();
     }
     public abstract void showMenu();
     public abstract void controlChoice(int choice);
     public final void run() {
-    	showMenu();
-    	int choice=consoleUI.getMenuChoice();
-    	controlChoice(choice);
+        showMenu();
+        int choice = consoleUI.getMenuChoice();
+        controlChoice(choice);
     }
 }
